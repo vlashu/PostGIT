212deabf955df1b816c8278253e89833f2c4c78f
vlashu <93687317+vlashu@users.noreply.github.com>
2022-03-18 13:57:55 +0300

    correct function -> func

--- include/comparison.py
+++ include/comparison.py
@@ -8,7 +8,7 @@ def object_type_to_function_comparison(object_type) -> str:
         'view': 'pg_get_viewdef',             # pg_get_viewdef ( view oid [, pretty boolean ] ) → text
         'index': 'pg_get_indexdef',           # pg_get_indexdef ( index oid [, column integer, pretty boolean ] ) → text
         'constraint': 'pg_get_constraintdef', # pg_get_constraintdef ( constraint oid [, pretty boolean ] ) → text
-        'function': 'pg_get_functiondef',     # pg_get_functiondef ( func oid ) → text
+        'func': 'pg_get_functiondef',     # pg_get_functiondef ( func oid ) → text
         'rule': 'pg_get_ruledef',             # pg_get_ruledef ( rule oid [, pretty boolean ] ) → text
         'trigger': 'pg_get_triggerdef',       # pg_get_triggerdef ( trigger oid [, pretty boolean ] ) → text
     }
